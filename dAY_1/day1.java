7c01d346f5bffdbfa433d655df5fb042f085ef3b
Jyothik6 <jyo842776@gmail.com>
2022-03-07 09:29:59 +0530

    Added Descriptions for Milestone-2,#2

---
+++ dAY_1/day1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class day1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the team name:\r");
+        String x = sc.nextLine();
+        System.out.println("Enter session:\r");
+        String y=sc.nextLine();
+        String a= " to win";
+        System.out.println("Enter runs:\r");
+        int z=sc.nextInt();
+        if(y.equals("First") || y.equals("first"))
+        {
+            ++z;
+            System.out.println("Name:" + x);
+            System.out.println("Scored:" + y);
+            System.out.println("Need " + z + a);
+        }
+       else if(y.equals("second") || y.equals("Second")){
+           System.out.println("Name:" + x);
+           System.out.println("Scored:" + y);
+           System.out.println("Matched Ended.");
+       }
+       else{
+        System.out.println("Invalid Input.");
+       }
+    }
+
+}
+    
+
